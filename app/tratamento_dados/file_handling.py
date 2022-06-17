5f2a3512d3b30e654f934444589b80cbe7402396
Rafhael de Oliveira Martins <rafhaeldeoliveira@gmail.com>
2022-06-17 00:47:21 -0300

    :recycle: rename: Arquivos renomeados.

--- app/tratamento_dados/file_handling.py
+++
@@ -1,9 +0,0 @@
-
-class TratamentoArquivo:
-    def __init__(self) -> None:
-        self.path_raw = 'C:/Users/Usuario/Documents/Projetos_git/iesb_pi6/dados/raw_data/'
-        self.path_base = 'C:/Users/Usuario/Documents/Projetos_git/iesb_pi6/dados/base_data/'
-
-        # Dicionário com a chave (nome do mês) e valor (mês em número).
-        self.meses = {'Jan': 1, 'Fev': 2, 'Mar': 3, 'Abr': 4, 'Mai': 5, 'Jun': 6, \
-            'Jul': 7, 'Ago': 8, 'Set': 9, 'Out': 10, 'Nov': 11, 'Dez': 12}
